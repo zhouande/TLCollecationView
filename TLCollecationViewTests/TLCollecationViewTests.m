90cf6a9f70a8e76caabe9555c02f48efafe43fcc
andezhou <zhoujunfeng007@126.com>
2015-07-28 14:50:41 +0800

    Initial Commit

---
+++ TLCollecationViewTests/TLCollecationViewTests.m
@@ -0,0 +1,40 @@
+//
+//  TLCollecationViewTests.m
+//  TLCollecationViewTests
+//
+//  Created by andezhou on 15/7/28.
+//  Copyright (c) 2015年 andezhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TLCollecationViewTests : XCTestCase
+
+@end
+
+@implementation TLCollecationViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
