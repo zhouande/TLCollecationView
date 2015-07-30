08b913e9b585b2145a274df06e426684c806e10a
andezhou <zhoujunfeng007@126.com>
2015-07-30 15:40:10 +0800

    first commit

--- TLCollecationView/TLCollectionViewCell.m
+++ TLCollecationView/TLCollectionViewCell.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  TLCollectionViewCell.m
 //  TLCollecationView
 //
 //  Created by andezhou on 15/7/28.
 //  Copyright (c) 2015年 andezhou. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "TLCollectionViewCell.h"
 
-@interface ViewController : UIViewController
-
+@implementation TLCollectionViewCell
 
 @end
-
---
+++ TLCollecationView/TLCollectionViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  TLCollectionViewFlowLayout.m
+//  TLCollecationView
+//
+//  Created by andezhou on 15/7/28.
+//  Copyright (c) 2015年 andezhou. All rights reserved.
+//
+
+#import "TLCollectionViewFlowLayout.h"
+
+@implementation TLCollectionViewFlowLayout
+
+@end
